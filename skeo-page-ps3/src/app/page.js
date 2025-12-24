dbc5fe7262d7dc362de1508d3d172693c41f95ca
Skeo <skeo051600@gmail.com>
2025-12-23 21:20:49 -0600

    additional horizontal xmb logic -- adjusted LoadXmbIcon component in main_array.js to include iconObj prop, using iconObj.image for src
    -- mapped through xmbicons arrays in page.js for
    LoadXmbIcon component
    
    -- gave id to each icon in xmbicons array for
    unique key in LoadXmbIcon

--- skeo-page-ps3/src/app/page.js
+++ skeo-page-ps3/src/app/page.js
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { xmbIcons } from "../../xmb_icon_arrays/main_array_data.js";
 
 import LoadXmbIcons from "../../components/main_array";
 
+
 export default function Home() {
   return (
-    <LoadXmbIcons />
+
+
+    <div className="XMB-horizontal">
+      {xmbIcons.map((icon) => (
+        <LoadXmbIcons key={icon.id} iconObj={icon} />
+      ))}
+    </div>
   );
 }
