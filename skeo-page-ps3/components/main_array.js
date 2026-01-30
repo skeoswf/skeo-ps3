124a983d76788cd936f7657ff020591032c15310
Skeo <skeo051600@gmail.com>
2026-01-29 21:03:13 -0600

    add touch interaction and transition to icons
    added handleiconclick in home function
    added onclick to icons
    
    videos now start at 20% volume
    gave icon hover a transition effect

--- skeo-page-ps3/components/main_array.js
+++ skeo-page-ps3/components/main_array.js
@@ -1,18 +1,25 @@
 import Image from "next/image";
 
-function LoadXmbIcons({ iconObj }) {
+function LoadXmbIcons({ iconObj, onClick }) {
+
+
   return (
-    <div className={`icon-square ${iconObj.active ? "active-icon" : ""}`}>
+    <div
+      className={`icon-square ${iconObj.active ? "active-icon" : ""}`}
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      style={{ cursor: "pointer" }}
+    >
       <Image
         src={iconObj.image}
         alt={iconObj.text}
         width={120}
         height={120}
         className="icon-image"
-        unoptimized={true}
+        unoptimized
       />
       <span className="icon-square-text">{iconObj.text}</span>
-
     </div>
   );
 }
