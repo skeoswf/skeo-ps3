ff17e36f18639085fb598493ce5c402177767ea1
Skeo <skeo051600@gmail.com>
2026-02-09 01:29:14 -0600

    infobox now correctly calls weather api with geo location data -- need to get time and date next -- need to design and stylize
    -- what am i gonna do with the icons?
    
    the key is kinda public but lowk i dont care here

--- skeo-page-ps3/components/infobox_content.js
+++ skeo-page-ps3/components/infobox_content.js
@@ -1,12 +1,34 @@
+import useSWR from "swr";
+
 
 function InfoboxContent() {
 
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data: geoData, error: geoError } = useSWR("/api/geolib", fetcher);
+
+  let roughLocation = geoData?.city
+
+  const { data: weatherData, error: weatherError } = useSWR(`http://api.weatherapi.com/v1/forecast.json?key=7de18e37cf0249249d274829260102&q=${roughLocation}&days=1&aqi=no&alerts=no`, fetcher);
+
+  // for now i dont care if the key is exposed, since it's a free api and the key is rate limited. if this becomes a problem in the future, we can do things proper and create an api route that fetches the weather data and keeps the key secret. but for now this is fine and simpler.
+
+  if (weatherError) return <div>Failed to load weather data.</div>;
+  if (geoError) return <div>Failed to load geolocation data.</div>;
+  if (!weatherData) return <div>Loading weather data...</div>;
+
+  let currentWeather = weatherData.current.condition.text;
+  let currentTemp = weatherData.current.temp_f;
+
   return (
     <div id="infobox-content">
-      <p className="infobox-date">august 12th, 2013</p>
-      <p className="infobox-time">8:22 pm</p>
+      <p className="infobox-date info-p">{currentWeather.toLowerCase()}</p>
+      <p className="infobox-temp info-p">{currentTemp}°F</p>
+      <p className="infobox-time info-p">8:22 pm</p>
     </div>
   );
 }
 
 export default InfoboxContent;
+
+// i think we should just do time and weather. the location isnt that important and it can be wrong. we'll get their ip, use the geolocation api, and then get their rough location. the location we get isnt too accurate, but the weather we get from that location is usually pretty accurate. so we can just show the weather and time for that location, and not show the location itself. that way we avoid showing wrong information and we still provide useful information to the user.
